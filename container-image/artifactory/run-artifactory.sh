3e6d9919e86a11dce28700b252ffb9ce07b44749
Christoph Görn <goern@redhat.com>
2017-02-16 17:28:20 +0100

    fixed path to db config

--- container-image/artifactory/run-artifactory.sh
+++ container-image/artifactory/run-artifactory.sh
@@ -7,7 +7,7 @@ env
 
 ln -sf /opt/jfrog/artifactory/bin /var/opt/jfrog/artifactory/bin
 
-cat >> /opt/jfrog/artifactory/etc/storage.properties <<-EOF
+cat >> /opt/jfrog/artifactory/etc/db.properties <<-EOF
 type=postgresql
 driver=org.postgresql.Driver
 url=jdbc:postgresql://${ARTIFACTORY_POSTGRESQL_SERVICE_HOST}:${ARTIFACTORY_POSTGRESQL_SERVICE_PORT}/${DATABASE_NAME}
@@ -16,7 +16,7 @@ password=${DATABASE_PASSWORD}
 EOF
 
 echo "Current storage configuration"
-cat /opt/jfrog/artifactory/etc/storage.properties
+cat /opt/jfrog/artifactory/etc/db.properties
 
 cd /var/opt/jfrog/artifactory/bin/
 ./artifactory.sh
